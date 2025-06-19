8cbedbab6a73a87f65746d1ee56fddda17e865ed
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-06-19 08:58:15 +0700

    feat: adding dynamic metadata

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: `${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_REF}.supabase.co`,
+        port: '',
+        pathname: '/storage/v1/object/public/user-images/**',
+      },
+    ],
+  },
+};
+module.exports = nextConfig;
